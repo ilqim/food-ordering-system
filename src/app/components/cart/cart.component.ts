41888e06b38dc5b92c3f2071fd1168a55f89fd38
İlkim Eren <ilkimeren25@gmail.com>
2025-06-18 23:02:37 +0300

    chore: clean up file headers

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,4 +1,3 @@
-// src/app/components/cart/cart.component.ts
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
